a7ac0ed67b3e1d1265af07bddc6905389546817a
Beyza0000 <beyzahacat@outlook.com>
2024-12-05 02:27:56 +0300

    Update pengu.py

--- pengu.py
+++ pengu.py
@@ -4,22 +4,21 @@ import streamlit_pandas as sp
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv(file)
+    df = pd.read_csv(url, index_col=0)
     return df
 
-file = "titanic.csv"
+url = 'https://raw.githubusercontent.com/Beyza0000/byz/refs/heads/main/penguins_examples.csv'
 df = load_data()
-create_data = {"Name": "text",
-                "Sex": "multiselect",
-                "Embarked": "multiselect",
-                "Ticket": "text",
-                "Pclass": "multiselect"}
+create_data = {"studyName": "text",
+                "Sample": "multiselect",
+                "Number": "multiselect",
+        }
 
-all_widgets = sp.create_widgets(df, create_data, ignore_columns=["PassengerId"])
+all_widgets = sp.create_widgets(df, create_data, ignore_columns=["Individual ID"])
 res = sp.filter_df(df, all_widgets)
 st.title("Streamlit AutoPandas")
-st.header("Original DataFrame")
+st.header(" DataFrame")
 st.write(df)
 
-st.header("Result DataFrame")
+st.header("Result")
 st.write(res)
