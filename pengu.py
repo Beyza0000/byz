eb409fefb0bb6856cae3b7df34deab4796f043bc
Beyza0000 <beyzahacat@outlook.com>
2024-12-05 02:30:00 +0300

    Update pengu.py

--- pengu.py
+++ pengu.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-import streamlit_pandas as sp
+
 
 @st.cache_data
 def load_data():
@@ -9,15 +9,7 @@ def load_data():
 
 url = 'https://raw.githubusercontent.com/Beyza0000/byz/refs/heads/main/penguins_examples.csv'
 df = load_data()
-create_data = {"studyName": "text",
-                "Sample": "multiselect",
-                "Number": "multiselect",
-        }
 
-all_widgets = sp.create_widgets(df, create_data, ignore_columns=["Individual ID"])
-res = sp.filter_df(df, all_widgets)
-st.title("Streamlit AutoPandas")
-st.header(" DataFrame")
 st.write(df)
 
 st.header("Result")
