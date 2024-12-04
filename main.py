ea7ba1a9fac021117be5248c892ecd9a622d7322
Beyza0000 <beyzahacat@outlook.com>
2024-12-04 23:48:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 import streamlit as st
-st.write("hello")
+import pandas as pd
+st.title("data dashboard")
+uploaded_file = st.file_uploader("please upload your CSV file")
+if uploaded_file:
+  df = pd.read_csv(uploaded_file)
+  st.subheader("put your dataset: ")
+  st.write(df.head())
+  st.subheader("filter: ")
+  columns= df.columns.tolist()
+  selected_column= st.selectbox("choose column", columns)
+  filter_value = st.text_input("bir deger giriniz")
+  if selected_column and filter_value:
+    filtered_df = df[df[selected_column].astype(str) == filter_value]
+    st.write(filtered_df)
+  
+  
