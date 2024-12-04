4b657f17f0f6b9ee41ca00698cba5350e025f9bf
Beyza0000 <beyzahacat@outlook.com>
2024-12-05 02:19:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import streamlit as st
 import pandas as pd
-st.title("data dashboard")
-uploaded_file = st.file_uploader("please upload your CSV file")
+st.title("veri")
+uploaded_file = st.file_uploader(" dosyanızı giriniz")
 if uploaded_file:
   df = pd.read_csv(uploaded_file)
-  st.subheader("put your dataset: ")
+  st.subheader("Veri Dosyanız: ")
   st.write(df.head())
   st.subheader("filter: ")
   columns= df.columns.tolist()
-  selected_column= st.selectbox("choose column", columns)
-  filter_value = st.text_input("bir deger giriniz")
+  selected_column= st.selectbox("kolon", columns)
+  filter_value = st.text_input("veri girişi")
   if selected_column and filter_value:
     filtered_df = df[df[selected_column].astype(str) == filter_value]
     st.write(filtered_df)
 
-st.subheader("gorsellestırme:")
+st.subheader("gorsellestırme: ")
 x_column = st.selectbox("x ekseni için", columns)
 y_column = st.selectbox("y ekseni icin", columns)
 button= st.button("gorsellestr")
